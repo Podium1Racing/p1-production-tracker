21e356b8539eacf80fcb61cad2e966b9929defee
Elijah-Moosekian <elijah@podium1racing.com>
2026-08-06 15:05:07 -0500

    Delegate iOS standalone login selector taps

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const PT_SW_VERSION = "p0-login-advance-2026-08-06-1";
+const PT_SW_VERSION = "p0-login-delegated-2026-08-06-1";
 const PUSH_USER_CACHE = "pt-push-meta";
 const PUSH_USER_KEY = "/__push_user__";
 
